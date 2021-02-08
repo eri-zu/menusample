334de3de8308496aa7b40d6681fbaed5743b5905
eri1221 <izutsu812@gmail.com>
2021-02-08 16:05:15 +0900

    fix

--- js/main.js
+++ js/main.js
@@ -5,8 +5,6 @@ class Controller {
     this.drawer = document.querySelector(".js-drawer");
     this.drawerWidth = this.drawer.clientWidth;
     this.duration = 0.5; //イージングの時間
-    this.lastWidth = window.innerWidth; // 当初のウィンドウサイズ
-    this.bp = 750; // ブレイクポイント
 
     // flag
     this.isOpen = false;
@@ -17,38 +15,36 @@ class Controller {
   }
 
   setup() {
+    this.watchDrawerWidth();
+
     TweenMax.set(this.drawer, {
-      x: this.drawerWidth, // transform = translateX(this.drawerWidth)のことです！
+      x: this.drawerWidth,
     });
 
-    // ブレイクポイント通過した時、drawerの位置セットし直す
     window.addEventListener("resize", () => {
-      const curretWidth = window.innerWidth;
-      this.drawerWidth = this.drawer.clientWidth;
-
-      if (this.lastWidth <= this.bp && this.bp < this.curretWidth) {
-        // pc
-        TweenMax.set(this.drawer, {
-          x: this.drawerWidth,
-        });
-      } else if (this.currentWidth <= this.bp && this.bp < this.lastWidth) {
-        // sp
-        TweenMax.set(this.drawer, {
-          x: this.drawerWidth,
-        });
-      }
+      TweenMax.set(this.drawer, {
+        x: this.drawerWidth,
+      });
     });
   }
 
   show() {
     TweenMax.to(this.drawer, this.duration, {
-      x: 0, // transform = translateX(0)のことです！
+      x: 0,
     });
   }
 
   hide() {
+    this.watchDrawerWidth();
+
     TweenMax.to(this.drawer, this.duration, {
       x: this.drawerWidth,
+    });
+  }
+
+  watchDrawerWidth() {
+    window.addEventListener("resize", () => {
+      this.drawerWidth = this.drawer.clientWidth;
     });
   }
 
@@ -61,8 +57,6 @@ class Controller {
         this.show();
         this.isOpen = true;
       }
-
-      console.log(this.drawerWidth);
     });
   }
 }
